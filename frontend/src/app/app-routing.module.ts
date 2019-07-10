8bdeb8f83c4e05aa0419916c1e7780d872ab4f3e
Caleb Oki <caleboki@gmail.com>
2019-07-10 19:04:32 +0100

    Angular routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    //canActivate: [BeforeLoginService]
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    //canActivate: [BeforeLoginService]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    //canActivate: [AfterLoginService]
+  },
+  {
+    path: 'request-password-reset',
+    component: RequestResetComponent,
+    //canActivate: [BeforeLoginService]
+  },
+  {
+    path: 'response-password-reset',
+    component: ResponseResetComponent,
+    //canActivate: [BeforeLoginService]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
